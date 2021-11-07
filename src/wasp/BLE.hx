c126bfc914fe37524a4a2798f449020180a531ca
k <k@klabz.org>
2021-11-07 14:19:06 +0100

    About: disable ble mac address
    Importing ble module breaks BLE connection, making wasptool, gadgetbridge etc.
    unable to communicate with our watches.
    
    Only importing ble.address doesn't work either.

--- src/wasp/BLE.hx
+++ src/wasp/BLE.hx
@@ -1,12 +1,15 @@
 package wasp;
 
-#if simulator
+// TODO: enable ble mac address again when a workaround is found
+// See https://github.com/daniel-thompson/wasp-os/issues/271#issuecomment-962613262
+// #if simulator
 class BLE {
 	public static function address():String return "12:34:56:78:90:AB";
 }
-#else
-@:pythonImport('ble')
-extern class BLE {
-	static function address():String;
-}
-#end
+// #else
+// @:native('BLE_address')
+// @:pythonImport('ble', 'address')
+// extern class BLE {
+// 	@:selfCall static function address():String;
+// }
+// #end
