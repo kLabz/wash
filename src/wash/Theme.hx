015790dafea5e4270c43c28e5bcec4778509d9c3
k <k@klabz.org>
2021-11-09 22:09:31 +0100

    Rename Int2 to Int16

--- src/wash/Theme.hx
+++ src/wash/Theme.hx
@@ -1,48 +1,49 @@
 package wash;
 
-import wash.util.Int2;
 import python.Bytearray;
 
+import wash.util.Int16;
+
 abstract Theme(Bytearray) from Bytearray {
-	public var primary(get, set):Int2;
-	inline function get_primary():Int2 return Wash.system.nightMode ? primary_night : get(0);
-	inline function set_primary(value:Int2):Int2 return setColor(0, value);
+	public var primary(get, set):Int16;
+	inline function get_primary():Int16 return Wash.system.nightMode ? primary_night : get(0);
+	inline function set_primary(value:Int16):Int16 return setColor(0, value);
 
-	public var secondary(get, set):Int2;
-	inline function get_secondary():Int2 return Wash.system.nightMode ? secondary_night : get(1);
-	inline function set_secondary(value:Int2):Int2 return setColor(1, value);
+	public var secondary(get, set):Int16;
+	inline function get_secondary():Int16 return Wash.system.nightMode ? secondary_night : get(1);
+	inline function set_secondary(value:Int16):Int16 return setColor(1, value);
 
-	public var highlight(get, set):Int2;
-	inline function get_highlight():Int2 return Wash.system.nightMode ? highlight_night : get(2);
-	inline function set_highlight(value:Int2):Int2 return setColor(2, value);
+	public var highlight(get, set):Int16;
+	inline function get_highlight():Int16 return Wash.system.nightMode ? highlight_night : get(2);
+	inline function set_highlight(value:Int16):Int16 return setColor(2, value);
 
-	public var shadow(get, set):Int2;
-	inline function get_shadow():Int2 return get(3);
-	inline function set_shadow(value:Int2):Int2 return setColor(3, value);
+	public var shadow(get, set):Int16;
+	inline function get_shadow():Int16 return get(3);
+	inline function set_shadow(value:Int16):Int16 return setColor(3, value);
 
-	public var primary_theme(get, never):Int2;
-	inline function get_primary_theme():Int2 return get(0);
+	public var primary_theme(get, never):Int16;
+	inline function get_primary_theme():Int16 return get(0);
 
-	public var secondary_theme(get, never):Int2;
-	inline function get_secondary_theme():Int2 return get(1);
+	public var secondary_theme(get, never):Int16;
+	inline function get_secondary_theme():Int16 return get(1);
 
-	public var highlight_theme(get, never):Int2;
-	inline function get_highlight_theme():Int2 return get(2);
+	public var highlight_theme(get, never):Int16;
+	inline function get_highlight_theme():Int16 return get(2);
 
-	public var primary_night(get, never):Int2;
-	inline function get_primary_night():Int2 return get(4);
+	public var primary_night(get, never):Int16;
+	inline function get_primary_night():Int16 return get(4);
 
-	public var secondary_night(get, never):Int2;
-	inline function get_secondary_night():Int2 return get(5);
+	public var secondary_night(get, never):Int16;
+	inline function get_secondary_night():Int16 return get(5);
 
-	public var highlight_night(get, never):Int2;
-	inline function get_highlight_night():Int2 return get(6);
+	public var highlight_night(get, never):Int16;
+	inline function get_highlight_night():Int16 return get(6);
 
-	function get(index:Int):Int2 {
+	function get(index:Int):Int16 {
 		return this[index * 2] << 8 | this[index * 2 + 1];
 	}
 
-	function setColor(index:Int, value:Int2):Int2 {
+	function setColor(index:Int, value:Int16):Int16 {
 		this[index * 2] = value >> 8;
 		this[index * 2 + 1] = value - ((value >> 8) << 8);
 		return value;
--- src/wash/util/Int16.hx
+++ src/wash/util/Int16.hx
@@ -1,13 +1,13 @@
 package wash.util;
 
-abstract Int2(Int) from Int to Int {
+abstract Int16(Int) from Int to Int {
 	public var _1(get,never):Int;
 	function get__1():Int return this >> 8;
 
 	public var _2(get,never):Int;
 	function get__2():Int return this - (_1 << 8);
 
-	public static function fromBytes(b1:Int, b2:Int):Int2 {
-		return (b1 << 8 | b2 :Int2);
+	public static function fromBytes(b1:Int, b2:Int):Int16 {
+		return (b1 << 8 | b2 :Int16);
 	}
 }
