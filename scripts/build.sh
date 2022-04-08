077f564da88247dd8b866dc983f48496aa00ccf5
k <k@klabz.org>
2022-04-08 11:16:25 +0200

    [CI] Add post-processing

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./haxe/haxe build.hxml
+
+python --version
+
+pip install astunparse
+
+pip3 install astunparse
+
+python3 -m astunparse
+
+python -m astunparse
+
+python ./scripts/postprocess.py
+
+python3 ./scripts/postprocess.py
+
